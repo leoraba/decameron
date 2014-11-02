33c882395c8adb8b71fc1f9ee787b0019c25972f
Leonardo Rivera <leorivera_88@hotmail.com>
2014-11-02 15:33:59 -0600

    Pantalla de muestra del modulo de factura y de estadisticas

--- modulos/facturacion.php
+++ modulos/facturacion.php
@@ -16,3 +16,97 @@
 </div>
 
 <!-- contenido -->
+<div id="divSearch">
+    <div class="row">
+        <div class="col-lg-6">
+            <label class="control-label">Busqueda:</label>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="form-group input-group">
+                <input type="text" class="form-control" placeholder="nombre de cliente o # de habitacion">
+                <span class="input-group-btn"><button type="button" class="btn btn-default" id="btnBuscar"><i class="fa fa-search"></i></button></span>
+            </div>
+        </div>
+    </div>
+</div>
+<form role="form">
+    <div class="panel panel-green" id="divInfo" style="display: none">
+        <div class="panel-heading">
+            <h3 class="panel-title">Detalle de factura</h3>
+        </div>
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-lg-9">
+                </div>
+                <div class="col-lg-3">
+                    <button class="btn btn-lg btn-success" type="button">Efectuar pago</button>
+                </div>
+            </div>
+            <div class="">
+                <div class="row">
+                    <div class="col-lg-6">
+                    </div>
+                    <div class="col-lg-6">
+                            <h1>Factura # <b>28883</b></h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <b>Ciudad y fecha:</b>Sonsonate, Sabado 01 de noviembre del 2014<br><br>
+                    </div>
+                </div>
+            </div>
+            <div class="table-responsive">
+                <table class="table table-bordered table-hover table-striped">
+                    <thead>
+                        <tr>
+                            <th># de Orden</th>
+                            <th>Detalle</th>
+                            <th>Monto (USD)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>3326</td>
+                            <td>Hospedaje 2 adultos 1 ni&ntilde;o</td>
+                            <td>$321.33</td>
+                        </tr>
+                        <tr>
+                            <td>3451</td>
+                            <td>1 servicio de spa</td>
+                            <td>$19.25</td>
+                        </tr>
+                        <tr>
+                            <td>3502</td>
+                            <td>1 alquiler de bicicleta</td>
+                            <td>$2.00</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="row well">
+                <div class="col-lg-6">
+                    <h3>Subtotal:</h3>
+                    <h3>IVA:</h3>
+                    <h1>Total:</h1>
+                </div>
+                <div class="col-lg-6">
+                    <h3>$342.58</h3>
+                    <h3>$44.53</h3>
+                    <h1>$387.11</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+<form>
+
+<script type="text/javascript" language="Javascript">
+    $(document).ready(function(){
+        $("#btnBuscar").click(function(){
+           $("#divInfo").show();
+           $("#divSearch").hide();
+        });
+    });
+</script>
--- modulos/reportes_estadisticas.php
+++ modulos/reportes_estadisticas.php
@@ -1,3 +1,7 @@
+<!-- Morris Charts CSS -->
+<link href="css/plugins/morris.css" rel="stylesheet">
+
+
 <!-- Page Heading -->
 <div class="row">
     <div class="col-lg-12">
@@ -6,13 +10,232 @@
         </h1>
         <ol class="breadcrumb">
             <li>
-                <i class="fa fa-home"></i> <a href='?m=home'> Inicio</a>
+                <i class="fa fa-home">
+                </i>
+                 <a href='?m=home'>
+                 Inicio</a>
             </li>
             <li class="active">
-                <i class="fa fa-bar-chart-o"></i> Estad&iacute;sticas
+                <i class="fa fa-bar-chart-o">
+                </i>
+                 Estad&iacute;sticas
             </li>
         </ol>
     </div>
 </div>
 
+
 <!-- contenido -->
+<div class="row">
+    <div class="col-lg-6">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <h3 class="panel-title"><i class="fa fa-long-arrow-right"></i> Pais de origen</h3>
+            </div>
+            <div class="panel-body">
+                <div id="morris-bar-chart"></div>
+                <div class="text-right">
+                    <a href="#">Ver detalles <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6">
+        <div class="panel panel-green">
+            <div class="panel-heading">
+                <h3 class="panel-title"><i class="fa fa-long-arrow-right"></i> Rango de edad</h3>
+            </div>
+            <div class="panel-body">
+                <div id="morris-donut-chart"></div>
+                <div class="text-right">
+                    <a href="#">Ver detalles <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="panel panel-red">
+        <div class="panel-heading">
+            <h3 class="panel-title"><i class="fa fa-long-arrow-right"></i> Historial de visitas en el hotel</h3>
+        </div>
+        <div class="panel-body">
+            <div id="morris-line-chart"></div>
+            <div class="text-right">
+                <a href="#">Ver detalles <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Morris Charts JavaScript -->
+<script src="js/plugins/morris/raphael.min.js"></script>
+<script src="js/plugins/morris/morris.min.js"></script>
+<script style="text/javascript" language"JavaScript">
+$( document ).ready(function() {
+// Line Chart
+    Morris.Line({
+        // ID of the element in which to draw the chart.
+        element: 'morris-line-chart',
+        // Chart data records -- each entry in this array corresponds to a point on
+        // the chart.
+        data: [{
+            d: '2014-10-01',
+            visitas: 802
+        }, {
+            d: '2014-10-02',
+            visitas: 783
+        }, {
+            d: '2014-10-03',
+            visitas: 820
+        }, {
+            d: '2014-10-04',
+            visitas: 839
+        }, {
+            d: '2014-10-05',
+            visitas: 792
+        }, {
+            d: '2014-10-06',
+            visitas: 859
+        }, {
+            d: '2014-10-07',
+            visitas: 790
+        }, {
+            d: '2014-10-08',
+            visitas: 1680
+        }, {
+            d: '2014-10-09',
+            visitas: 1592
+        }, {
+            d: '2014-10-10',
+            visitas: 1420
+        }, {
+            d: '2014-10-11',
+            visitas: 882
+        }, {
+            d: '2014-10-12',
+            visitas: 889
+        }, {
+            d: '2014-10-13',
+            visitas: 819
+        }, {
+            d: '2014-10-14',
+            visitas: 849
+        }, {
+            d: '2014-10-15',
+            visitas: 870
+        }, {
+            d: '2014-10-16',
+            visitas: 1063
+        }, {
+            d: '2014-10-17',
+            visitas: 1192
+        }, {
+            d: '2014-10-18',
+            visitas: 1224
+        }, {
+            d: '2014-10-19',
+            visitas: 1329
+        }, {
+            d: '2014-10-20',
+            visitas: 1329
+        }, {
+            d: '2014-10-21',
+            visitas: 1239
+        }, {
+            d: '2014-10-22',
+            visitas: 1190
+        }, {
+            d: '2014-10-23',
+            visitas: 1312
+        }, {
+            d: '2014-10-24',
+            visitas: 1293
+        }, {
+            d: '2014-10-25',
+            visitas: 1283
+        }, {
+            d: '2014-10-26',
+            visitas: 1248
+        }, {
+            d: '2014-10-27',
+            visitas: 1323
+        }, {
+            d: '2014-10-28',
+            visitas: 1390
+        }, {
+            d: '2014-10-29',
+            visitas: 1420
+        }, {
+            d: '2014-10-30',
+            visitas: 1529
+        }, {
+            d: '2014-10-31',
+            visitas: 1892
+        }, ],
+        // The name of the data record attribute that contains x-visitass.
+        xkey: 'd',
+        // A list of names of data record attributes that contain y-visitass.
+        ykeys: ['visitas'],
+        // Labels for the ykeys -- will be displayed when you hover over the
+        // chart.
+        labels: ['visitas'],
+        // Disables line smoothing
+        smooth: false,
+        resize: true
+    });
+
+    // Bar Chart
+    Morris.Bar({
+        element: 'morris-bar-chart',
+        data: [{
+            device: 'Guatemala',
+            geekbench: 136
+        }, {
+            device: 'Honduras',
+            geekbench: 137
+        }, {
+            device: 'Australia',
+            geekbench: 275
+        }, {
+            device: 'Colombia',
+            geekbench: 380
+        }, {
+            device: 'Estados unidos',
+            geekbench: 655
+        }, {
+            device: 'El Salvador',
+            geekbench: 1571
+        }],
+        xkey: 'device',
+        ykeys: ['geekbench'],
+        labels: ['Geekbench'],
+        barRatio: 0.4,
+        xLabelAngle: 35,
+        hideHover: 'auto',
+        resize: true
+    });
+
+     // Donut Chart
+    Morris.Donut({
+        element: 'morris-donut-chart',
+        data: [{
+            label: "0-12 años",
+            value: 12
+        }, {
+            label: "13-18 años",
+            value: 30
+        }, {
+            label: "19-30 años",
+            value: 40
+        }, {
+            label: "31-50 años",
+            value: 30
+        }, {
+            label: "51 años o más",
+            value: 20
+        }],
+        resize: true
+    });
+});
+</script>
