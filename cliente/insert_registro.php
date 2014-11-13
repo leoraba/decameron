a5645591faf4f52de37167657c21bceb3d7c4634
Leonardo Rivera <leorivera_88@hotmail.com>
2014-11-12 23:44:06 -0600

    consulta insert de registro de cliente

--- cliente/insert_registro.php
+++ cliente/insert_registro.php
@@ -1,31 +1,24 @@
 <?php
-$con=mysqli_connect("107.170.230.218","isw123","isw123");
-mysql_select_db("decameron") or die('Error base de datos');
-// verificar conexion
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+include("../includes/conexion.php");
+$nombres = $_POST['nombres'];
+$apellidos = $_POST['apellidos'];
+$email = $_POST['email'];
+$genero = $_POST['genero'];
+$num_documento = $_POST['num_documento'];
+$fec_nacimiento = $_POST['fecha_nacimiento'];
+$contra = $_POST['contra'];
+$usuario = $_POST['usuario'];
+$cmbpais = $_POST['cmbPais'];
+$cmbtipodoc = $_POST['cmbTipoDocumento'];
+
+$sql = "INSERT INTO USUARIO (usuario, clave, estado, fecha_creacion, fk_id_rol) VALUES ('$usuario', '$contra', 'A','".date('Y-m-d H:i:s')."', '1')";
+
+if (mysql_query($sql,$ln)) {
+	$id=mysql_insert_id($ln);
+	$sq2 = "INSERT INTO CLIENTE_TITULAR (nombres, apellidos, email, genero, num_documento, fecha_nacimiento, fk_id_tipo_cliente, fk_id_tipo_documento, fk_id_pais, fk_id_usuario) VALUES ('$nombres', '$apellidos', '$email','$genero', '$num_documento', '$fec_nacimiento', '1','$cmbtipodoc','$cmbpais','$id')";
+	mysql_query($sq2,$ln);
+	echo "Usted se ha registrado exitosamente";
+}else {
+	echo "Error en el registro";
 }
-
-// escape variables for security
-$nombres = mysqli_real_escape_string($con, $_POST['nombres']);
-$apellidos = mysqli_real_escape_string($con, $_POST['apellidos']);
-$email = mysqli_real_escape_string($con, $_POST['email']);
-$genero = mysqli_real_escape_string($con, $_POST['genero']);
-$num_documento = mysqli_real_escape_string($con, $_POST['num_documento']);
-$fec_nacimiento = mysqli_real_escape_string($con, $_POST['fecha_nacimiento']);
-$contra = mysqli_real_escape_string($con, $_POST['contra']);
-$usuario = mysqli_real_escape_string($con, $_POST['usuario']);
-
-$sql = "INSERT INTO usuario (usuario, clave, estado, fecha_creacion, fk_id_rol) 
-VALUES ('$usuario', '$contra', 'A','".date('Y-m-d H:i:s')."', '1')";
-
-if (!mysqli_query($con,$sql)) {
-	$id=mysql_insert_id($con);
-	$sq2 = "INSERT INTO cliente_titular (nombres, apellidos, email, genero, num_documento, fecha_nacimiento, fk_id_tipo_cliente, fk_id_tipo_documento, fk_id_pais, fk_id_usuario) 
-VALUES ('$nombres', '$apellidos', '$email','$genero', '$num_documento', '$fec_nacimiento', '1','1','1','$usuario')";
-mysqli_query($con,$sq2);
-  die('Error: ' . mysqli_error($con));
-}
-echo "Usted se ha registrado exitosamente";
-mysqli_close($con);
 ?>
--- cliente/registro.php
+++ cliente/registro.php
@@ -1,3 +1,6 @@
+<?php
+include("../includes/conexion.php");
+?>
 <!doctype html> 
 <html>
 <head>
@@ -14,14 +17,40 @@
 <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <link rel="stylesheet" href="dist/css/bootstrapValidator.min.css"/>
+<link href="../css/datepicker.css" rel="stylesheet">
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
 <script type="text/javascript" src="dist/js/bootstrapValidator.min.js"></script>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
 <script src="js/validaciones.js"></script>
+<!-- Datepicker JavaScript -->
+<script src="../js/bootstrap-datepicker.js"></script>
 
   <script type="text/javascript" language="JavaScript">
-    var password1 = document.getElementById('password1');
-    var password2 = document.getElementById('password2');
+    $(document).ready(function(){
+      var password1 = document.getElementById('password1');
+      var password2 = document.getElementById('password2');
+
+      password1.addEventListener('change', checkPasswordValidity, false);
+      password2.addEventListener('change', checkPasswordValidity, false);
+
+      var form = document.getElementById('passwordForm');
+      form.addEventListener('submit', function() {
+          checkPasswordValidity();
+          if (!this.checkValidity()) {
+              event.preventDefault();
+              //Implement you own means of displaying error messages to the user here.
+              password1.focus();
+          }
+      }, false);
+
+      $('#fecha_nacimiento').datepicker({
+            format: "dd/mm/yyyy",
+            language: "es",
+            todayBtn: true,
+            autoclose: true,
+            todayHighlight: true
+      });
+    });
 
     var checkPasswordValidity = function() {
         if (password1.value != password2.value) {
@@ -30,19 +59,6 @@
             password1.setCustomValidity('');
         }        
     };
-
-    password1.addEventListener('change', checkPasswordValidity, false);
-    password2.addEventListener('change', checkPasswordValidity, false);
-
-    var form = document.getElementById('passwordForm');
-    form.addEventListener('submit', function() {
-        checkPasswordValidity();
-        if (!this.checkValidity()) {
-            event.preventDefault();
-            //Implement you own means of displaying error messages to the user here.
-            password1.focus();
-        }
-    }, false);
 </script>
 <style>
 #wrapper {
@@ -80,56 +96,70 @@
 <body>
       
 
-        <div class="container">  
-<a title="Inicio" href="index.php"><img src="img/logo.png" width="150" height="50" href:"index.php"/></a><br/>            
-  <h3>Crear tu cuenta</h3>
+  <div class="container">  
+    <a title="Inicio" href="index.php"><img src="img/logo.png" width="150" height="50" href:"index.php"/></a><br/>            
+    <h3>Crear tu cuenta</h3>
                 
-<form action="insert_registro.php" method="post">
-  <header>Nombres:</header>
-  <input type="text" name="nombres" maxlength="30" placeholder="Tus nombres">
-   <!---- Nombre---->
+    <form action="insert_registro.php" method="post" id="passwordForm">
+      <header>Nombres:</header>
+      <input type="text" name="nombres" maxlength="30" placeholder="Tus nombres">
 
-   <!---- Apellido---->
-<header>Apellidos:</header>
-    <input type="text" name="apellidos" maxlength="30" placeholder="Tus apellidos">
-   <!---- Apellido---->
+      <header>Apellidos:</header>
+      <input type="text" name="apellidos" maxlength="30" placeholder="Tus apellidos">
 
+      <header>Email:</header>
+      <input type="email" name="email" maxlength="30" placeholder="Tu correo electronico">
 
-   <!---- Email---->
-<header>Email:</header>
-    <input type="email" name="email" maxlength="30" placeholder="Tu correo electronico">
-  <!---- genero---->
+      <header>Genero:</header>
+      <input type="radio" name="genero" value="Hombre" checked="checked">Hombre<br>
+      <input type="radio" name="genero" value="Mujer">Mujer<br><br/>
 
-<header>Genero:</header>
-<input type="radio" name="genero" value="Hombre">Hombre<br>
-<input type="radio" name="genero" value="Mujer">Mujer<br><br/>
-  <!---- genero---->
+      <header>Pais:</header>
+      <select name="cmbPais" id="cmbPais">
+          <option value=""> Seleccione una opci&oacute;n</option>
+          <?php
+              $qr = mysql_query("SELECT id_pais, nombre_pais FROM PAIS ORDER BY nombre_pais ASC",$ln);
+              while($row = mysql_fetch_array($qr)){
+                  echo "<option value='".$row['id_pais']."'>".$row['nombre_pais']."</option>";
+              }
+          ?>
+      </select>
 
-<!---- Email---->
-<header># de documento:</header>
-    <input type="text" name="num_documento" maxlength="30" placeholder="# de documento">
-  <!---- genero---->
+      <header>Tipo de documento:</header>
+      <select name="cmbTipoDocumento" id="cmbTipoDocumento">
+          <option value=""> Seleccione una opci&oacute;n</option>
+          <?php
+              $qr = mysql_query("SELECT id_tipo_documento, tipo_documento FROM TIPO_DOCUMENTO ORDER BY tipo_documento ASC",$ln);
+              while($row = mysql_fetch_array($qr)){
+                  echo "<option value='".$row['id_tipo_documento']."'>".$row['tipo_documento']."</option>";
+              }
+          ?>
+      </select>
 
-  <!---- Email---->
-<header>Fecha de nacimiento:</header>
-<input type="date" name="fecha_nacimiento"><br/>
-  <!---- genero---->
+      <header># de documento:</header>
+      <input type="text" name="num_documento" maxlength="30" placeholder="# de documento">
 
-  <header>Usuario:</header>
-    <input type="text" name="usuario" maxlength="30" placeholder="Usuario">
-   <!---- Apellido---->
+      <header>Fecha de nacimiento:</header>
+      <div class='input-group date' id="fecha_nacimiento">
+          <input type='text' class="form-control" name="fecha_nacimiento" />
+          <span class="input-group-addon">
+              <span class="fa fa-calendar"></span>
+          </span>
+      </div>
 
-<header>Contrasena:</header>
-  <input type="password" name="contra" required id="password1"  pattern=".{8,}" required title="Tu contrasena debe tener al menos 8 caracteres" placeholder="8 caracteres minimo"/><br/>
+      <header>Usuario:</header>
+      <input type="text" name="usuario" maxlength="30" placeholder="Usuario">
+
+      <header>Contrasena:</header>
+      <input type="password" name="contra" required id="password1"  pattern=".{8,}" required title="Tu contrasena debe tener al menos 8 caracteres" placeholder="8 caracteres minimo"/><br/>
             
-<header>Repetir contrasena:</header>
-<input type="password" name="rcontra" required id="password2" pattern=".{8,}" required title="Tu contrasena debe tener al menos 8 caracteres" placeholder="8 caracteres minimo"/><br/> 
+      <header>Repetir contrasena:</header>
+      <input type="password" name="rcontra" required id="password2" pattern=".{8,}" required title="Tu contrasena debe tener al menos 8 caracteres" placeholder="8 caracteres minimo"/><br/> 
 
-  <input type="submit">
+      <input type="submit">
 
-    <a href="login.html">Ya tienes una cuenta? Iniciar sesion</a>
+      <a href="login.html">Ya tienes una cuenta? Iniciar sesion</a>
     </form>
-                                <!---- password validation---->
- 
+  </div> 
 </body>
 </html>     	
