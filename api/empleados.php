e594b6541e35ffcb2314c1f97e7fa08455c5c2bb
Leonardo Rivera <leorivera_88@hotmail.com>
2014-11-19 02:32:07 -0600

    modal modificar empleado

--- api/empleados.php
+++ api/empleados.php
@@ -1,9 +1,9 @@
 <?php
-include("../includes/conexion.php");
 if(isset($_REQUEST)){
+	include("../includes/conexion.php");
+	$success=false;
 	$accion=$_REQUEST['accion'];
 	if($accion=="nvo"){
-		$success=false;
 		$nombre=$_POST['txtNombre'];
 		$apellido=$_POST['txtApellido'];
 		$usuario=$_POST['txtUsuario'];
@@ -22,8 +22,35 @@ if(isset($_REQUEST)){
 			$idUsuario=mysql_insert_id($ln);
 			if(mysql_query("INSERT INTO EMPLEADO(nombres, apellidos, genero, fecha_nacimiento, telefono, email, estado_civil, numero_documento, fk_id_cargo, fk_id_pais, fk_id_tipo_documento) VALUES('$nombre','$apellido','$genero','$fNacimiento','$telefono','$email','$estadoCivil','$documento','$cargo','$pais','$tipoDocumento')",$ln)) $success=true;
 		}
+		$resp=array("success"=>$success);
+		echo json_encode($resp);
+	}else if($accion=="get"){
+		$id=$_REQUEST['id'];
+		$datos=array();
+		$qr=mysql_query("SELECT e.id_empleado, e.nombres, e.apellidos, e.genero, e.fecha_nacimiento, e.telefono, e.email, e.estado_civil, e.numero_documento, e.fk_id_cargo, e.fk_id_pais, e.fk_id_tipo_documento, u.usuario FROM EMPLEADO e LEFT JOIN USUARIO u ON e.fk_id_usuario=u.id_usuario WHERE e.id_empleado='$id'",$ln);
+		if(mysql_num_rows($qr)==1){
+			$row=mysql_fetch_array($qr);
+			$datos[]=array(
+				"id_empleado"=>$row['id_empleado'],
+				"nombres"=>$row['nombres'],
+				"apellidos"=>$row['apellidos'],
+				"genero"=>$row['genero'],
+				"fecha_nacimiento"=>$row['fecha_nacimiento'],
+				"telefono"=>$row['telefono'],
+				"email"=>$row['email'],
+				"estado_civil"=>$row['estado_civil'],
+				"numero_documento"=>$row['numero_documento'],
+				"fk_id_cargo"=>$row['fk_id_cargo'],
+				"fk_id_pais"=>$row['fk_id_pais'],
+				"fk_id_tipo_documento"=>$row['fk_id_tipo_documento'],
+				"usuario"=>$row['usuario']
+			);
+		}
+		$resp=array("success"=>true,"datos"=>$datos);
+		echo json_encode($resp);
 	}else if($accion=="mod"){
-
+		$resp=array("success"=>true,"datos"=>"si");
+		echo json_encode($resp);
 	}else if($accion=="elim"){
 		$id=$_REQUEST['id'];
 		$qr=mysql_query("SELECT fk_id_usuario FROM EMPLEADO WHERE id_empleado='$id'");
@@ -31,9 +58,10 @@ if(isset($_REQUEST)){
 		$idUsuario=$row['fk_id_usuario'];
 		if(mysql_query("DELETE FROM EMPLEADO WHERE id_empleado='$id'")){
 			mysql_query("DELETE FROM USUARIO WHERE id_usuario='$idUsuario'");
-			echo true;
+			$success=true;
 		}
-
+		$resp=array("success"=>$success);
+		echo json_encode($resp);
 	}
 }
 ?>
--- modulos/empleados.php
+++ modulos/empleados.php
@@ -1,6 +1,4 @@
-<!-- Bootbox js -->
-<script src="js/jquery.leanModal.min.js"></script>
-<link href="css/lean-modal.css" rel="stylesheet">
+
 
 <!-- Page Heading -->
 <div class="row">
@@ -20,133 +18,137 @@
 </div>
 
 <!-- contenido -->
-<!-- <button href="#divFormUsuario" class="btn btn-lg btn-primary" type="button" id="btnNuevo">Nuevo</button> -->
-<a id="btnNuevo" href="#divFormUsuario" name="divFormUsuario" rel="leanModal" class="btn btn-lg btn-primary">Nuevo</a>
+<a href="#divFormUsuario" name="divFormUsuario" class="btn btn-primary" id="btnNuevo"> <i class="fa fa-plus"></i> Nuevo </a>
 
 
-<div id="divFormUsuario" style="display: none; ">
-        <div id="leanModal-header">
-            <h2>Nuevo empleado</h2>
-            <a href="#" class="modal_close"></a>
-        </div>
-        <br>
-        <form role="form" id="manto_form" action="" method="POST">
-            <input type="hidden" name="accion" value="nvo">
-           <div class="row">
-                <div class="form-group col-lg-6">
-                    <label class="control-label" > Nombre *</label>
-                    <input class="form-control" placeholder="" name="txtNombre" id="txtNombre" >
-                </div>
-                <div class="form-group col-lg-6">
-                    <label class="control-label" > Apellido *</label>
-                    <input class="form-control" placeholder="" name="txtApellido" id="txtApellido" >
-                </div>
+<div id="divFormUsuario" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title">Empleado</h4>
             </div>
-            <div class="row">
-                <div class="form-group col-lg-6">
-                    <label class="control-label" > Usuario *</label>
-                    <input class="form-control" placeholder="" name="txtUsuario" id="txtUsuario" >
-                </div>
-                <div class="form-group col-lg-6">
-                    <label class="control-label" > Clave *</label>
-                    <input type="password" class="form-control" placeholder="" name="txtClave" id="txtClave" >
-                </div>
-            </div>
-            <div class="row">
-                <div class="form-group col-lg-6">
-                    <label class="control-label" > Genero *</label><br>
-                    <label class="radio-inline">
-                        <input type="radio" name="radGenero" id="inlineRadio1" value="M" checked="checked"> Masculino
-                    </label>
-                    <label class="radio-inline">
-                        <input type="radio" name="radGenero" id="inlineRadio2" value="F"> Femenino
-                    </label>
-                </div>
-                <div class="form-group col-lg-6">
-                    <label class="control-label" > Fecha de nacimiento *</label>
-                    <div class='input-group date' id="txtFechaNacimiento" >
-                        <input type='text' class="form-control cssData" name="txtFechaNacimiento" />
-                        <span class="input-group-addon">
-                            <span class="fa fa-calendar"></span>
-                        </span>
+            <div class="modal-body">
+                <form role="form" id="manto_form" action="" method="POST">
+                    <input type="hidden" name="accion" value="nvo">
+                    <div class="row">
+                        <div class="form-group col-lg-6">
+                            <label class="control-label" > Nombre *</label>
+                            <input class="form-control" placeholder="" name="txtNombre" id="txtNombre" >
+                        </div>
+                        <div class="form-group col-lg-6">
+                            <label class="control-label" > Apellido *</label>
+                            <input class="form-control" placeholder="" name="txtApellido" id="txtApellido" >
+                        </div>
+                        </div>
+                        <div class="row">
+                        <div class="form-group col-lg-6">
+                            <label class="control-label" > Usuario *</label>
+                            <input class="form-control" placeholder="" name="txtUsuario" id="txtUsuario" >
+                        </div>
+                        <div class="form-group col-lg-6">
+                            <label class="control-label" > Clave *</label>
+                            <input type="password" class="form-control" placeholder="" name="txtClave" id="txtClave" >
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="form-group col-lg-6">
-                    <label class="control-label" > Telefono</label>
-                    <input class="form-control" placeholder="" name="txtTelefono" id="txtTelefono" >
-                </div>
-                <div class="form-group col-lg-6">
-                    <label class="control-label" > Estado civil</label>
-                    <select class="form-control" name="cmbEstadoCivil" id="cmbEstadoCivil">
-                        <option value=""> Seleccione una opci&oacute;n</option>
-                        <option value="Soltero"> Soltero</option>
-                        <option value="Casado"> Casado</option>
-                        <option value="Acompañado"> Acompañado</option>
-                        <option value="Viudo"> Viudo</option>
-                    </select>
-                </div>
-            </div>
+                    <div class="row">
+                        <div class="form-group col-lg-6">
+                            <label class="control-label" > Genero *</label><br>
+                            <label class="radio-inline">
+                                <input type="radio" name="radGenero" id="inlineRadio1" value="M" checked="checked"> Masculino
+                            </label>
+                            <label class="radio-inline">
+                                <input type="radio" name="radGenero" id="inlineRadio2" value="F"> Femenino
+                            </label>
+                        </div>
+                        <div class="form-group col-lg-6">
+                            <label class="control-label" > Fecha de nacimiento *</label>
+                            <div class='input-group date' id="txtFechaNacimiento" >
+                                <input type='text' class="form-control cssData" name="txtFechaNacimiento" />
+                                <span class="input-group-addon">
+                                    <span class="fa fa-calendar"></span>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-lg-6">
+                            <label class="control-label" > Telefono</label>
+                            <input class="form-control" placeholder="" name="txtTelefono" id="txtTelefono" >
+                        </div>
+                        <div class="form-group col-lg-6">
+                            <label class="control-label" > Estado civil</label>
+                            <select class="form-control" name="cmbEstadoCivil" id="cmbEstadoCivil">
+                                <option value=""> Seleccione una opci&oacute;n</option>
+                                <option value="Soltero"> Soltero</option>
+                                <option value="Casado"> Casado</option>
+                                <option value="Acompañado"> Acompañado</option>
+                                <option value="Viudo"> Viudo</option>
+                            </select>
+                        </div>
+                    </div>
 
-            <div class="row">
-                <div class="form-group col-lg-12">
-                    <label class="control-label" > Email</label>
-                    <input class="form-control" placeholder="" name="txtEmail" id="txtEmail" >
-                </div>
+                    <div class="row">
+                        <div class="form-group col-lg-12">
+                            <label class="control-label" > Email</label>
+                            <input class="form-control" placeholder="" name="txtEmail" id="txtEmail" >
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="form-group col-lg-6">
+                            <label class="control-label" > Documento</label>
+                            <select class="form-control" name="cmbTipoDocumento" id="cmbTipoDocumento">
+                                <option value=""> Seleccione una opci&oacute;n</option>
+                                <?php
+                                    $qr = mysql_query("SELECT id_tipo_documento, tipo_documento FROM TIPO_DOCUMENTO ORDER BY tipo_documento ASC",$ln);
+                                    while($row = mysql_fetch_array($qr)){
+                                        echo "<option value='".$row['id_tipo_documento']."'>".$row['tipo_documento']."</option>";
+                                    }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="col-lg-6">
+                            <label class="control-label" > &nbsp;</label>
+                            <input class="form-control" placeholder="" name="txtDocumento" id="txtDocumento" >
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-lg-6">
+                            <label class="control-label" > Cargo</label>
+                            <select class="form-control" name="cmbCargo" id="cmbCargo">
+                                <option value=""> Seleccione una opci&oacute;n</option>
+                                <?php
+                                    $qr = mysql_query("SELECT id_cargo, nombre_cargo FROM CARGO ORDER BY nombre_cargo ASC",$ln);
+                                    while($row = mysql_fetch_array($qr)){
+                                        echo "<option value='".$row['id_cargo']."'>".$row['nombre_cargo']."</option>";
+                                    }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="form-group col-lg-6">
+                            <label class="control-label" > Pais</label>
+                            <select class="form-control" name="cmbPais" id="cmbPais">
+                                <option value=""> Seleccione una opci&oacute;n</option>
+                                <?php
+                                    $qr = mysql_query("SELECT id_pais, nombre_pais FROM PAIS ORDER BY nombre_pais ASC",$ln);
+                                    while($row = mysql_fetch_array($qr)){
+                                        echo "<option value='".$row['id_pais']."'>".$row['nombre_pais']."</option>";
+                                    }
+                                ?>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-primary" type="button" id="btnGuardarEmpleado" name="btnGuardarEmpleado">
+                            <i class="fa fa-save"></i>
+                            Guardar
+                        </button>
+                    </div>
+                </form>
             </div>
-            
-            <div class="row">
-                <div class="form-group col-lg-6">
-                    <label class="control-label" > Documento</label>
-                    <select class="form-control" name="cmbTipoDocumento" id="cmbTipoDocumento">
-                        <option value=""> Seleccione una opci&oacute;n</option>
-                        <?php
-                            $qr = mysql_query("SELECT id_tipo_documento, tipo_documento FROM TIPO_DOCUMENTO ORDER BY tipo_documento ASC",$ln);
-                            while($row = mysql_fetch_array($qr)){
-                                echo "<option value='".$row['id_tipo_documento']."'>".$row['tipo_documento']."</option>";
-                            }
-                        ?>
-                    </select>
-                </div>
-                <div class="col-lg-6">
-                    <label class="control-label" > &nbsp;</label>
-                    <input class="form-control" placeholder="" name="txtDocumento" id="txtDocumento" >
-                </div>
-            </div>
-            <div class="row">
-                <div class="form-group col-lg-6">
-                    <label class="control-label" > Cargo</label>
-                    <select class="form-control" name="cmbCargo" id="cmbCargo">
-                        <option value=""> Seleccione una opci&oacute;n</option>
-                        <?php
-                            $qr = mysql_query("SELECT id_cargo, nombre_cargo FROM CARGO ORDER BY nombre_cargo ASC",$ln);
-                            while($row = mysql_fetch_array($qr)){
-                                echo "<option value='".$row['id_cargo']."'>".$row['nombre_cargo']."</option>";
-                            }
-                        ?>
-                    </select>
-                </div>
-                <div class="form-group col-lg-6">
-                    <label class="control-label" > Pais</label>
-                    <select class="form-control" name="cmbPais" id="cmbPais">
-                        <option value=""> Seleccione una opci&oacute;n</option>
-                        <?php
-                            $qr = mysql_query("SELECT id_pais, nombre_pais FROM PAIS ORDER BY nombre_pais ASC",$ln);
-                            while($row = mysql_fetch_array($qr)){
-                                echo "<option value='".$row['id_pais']."'>".$row['nombre_pais']."</option>";
-                            }
-                        ?>
-                    </select>
-                </div>
-            </div>
-            <div id="leanModal-header">
-                <button class="btn btn-primary" type="button" id="btnGuardarEmpleado" name="btnGuardarEmpleado">
-                    <i class="fa fa-save"></i>
-                    Guardar
-                </button>
-            </div>
-        </form>
+        </div>
+    </div>
 </div>
 
 
@@ -175,7 +177,7 @@
                 echo "<i class='fa fa-gear'></i> <span class='caret'></span>";
                 echo "</button>";
                 echo "<ul class='dropdown-menu pull-right' role='menu'>";
-                echo "<li><a href='#'>Modificar</a></li>";
+                echo "<li><a onClick=\"abrirModificar('".$row['id_empleado']."')\">Modificar</a></li>";
                 echo "<li><a href='#' onClick=\"eliminarRegistro('".$row['id_empleado']."')\">Eliminar</a></li>";
                 echo "</ul>";
                 echo "</div>";
@@ -197,9 +199,22 @@ $(document).ready(function() {
         autoclose: true,
         todayHighlight: true,
     });
-
-    $("a[rel*=leanModal]").leanModal({ top : 100, overlay : 0.4, closeButton: ".modal_close" });
-    $.fn.modal.Constructor.prototype.enforceFocus = function() {};
+    $("#btnNuevo").click(function(){
+        $("#txtNombre").val("");
+        $("#txtApellido").val("");
+        $("#txtUsuario").val("");
+        $("#txtClave").val("");
+        $("#inlineRadio1").prop("checked", true);
+        $("#txtFechaNacimiento").val("");
+        $("#txtTelefono").val("");
+        $("#cmbEstadoCivil").val("");
+        $("#txtEmail").val("");
+        $("#cmbTipoDocumento").val("");
+        $("#txtDocumento").val("");
+        $("#cmbCargo").val("");
+        $("#cmbPais").val("");
+        $("#divFormUsuario").modal('show');
+    });
     $("#btnGuardarEmpleado").click(function(){ guardarEmpleado(); });
 });
 
@@ -213,9 +228,12 @@ function guardarEmpleado(){
         url:url,
         type:'POST',
         data:data,
-        success:function(data){
-            $("#lean_overlay").trigger("click");
-            window.location.href="?m=emp";
+        success:function(res){
+            var obj = jQuery.parseJSON(res);
+            if(obj.success){
+                $("#lean_overlay").trigger("click");
+                window.location.href="?m=emp";
+            }
         }
     });
 }
@@ -227,11 +245,44 @@ function eliminarRegistro(id){
         url:url,
         type:'POST',
         data:data,
-        success:function(data){
-            bootbox.hideAll();
-            window.location.href="?m=emp";
+        success:function(res){
+            var obj = jQuery.parseJSON(res);
+            if(obj.success){
+                window.location.href="?m=emp";
+            }
         }
     });
+}
+
+function abrirModificar(id){
+    $("#divFormUsuario").modal('show');
+    var url = "api/empleados.php";
+    var data = "accion=get&id="+id;
+    $.ajax({
+        url:url,
+        type:'POST',
+        data:data,
+        success:function(res){
+            var obj = jQuery.parseJSON(res);
+            if(obj.success){
+                $("#txtNombre").val(obj.datos[0].nombres);
+                $("#txtApellido").val(obj.datos[0].apellidos);
+                $("#txtUsuario").val(obj.datos[0].usuario);
+                $("#txtClave").val();
+                if(obj.datos[0].genero=="M") $("#inlineRadio1").prop("checked", true);
+                else $("#inlineRadio2").prop("checked", false);
+                $("#txtFechaNacimiento").val(obj.datos[0].fecha_nacimiento);
+                $("#txtTelefono").val(obj.datos[0].telefono);
+                $("#cmbEstadoCivil").val(obj.datos[0].estado_civil);
+                $("#txtEmail").val(obj.datos[0].email);
+                $("#cmbTipoDocumento").val(obj.datos[0].fk_id_tipo_documento);
+                $("#txtDocumento").val(obj.datos[0].numero_documento);
+                $("#cmbCargo").val(obj.datos[0].fk_id_cargo);
+                $("#cmbPais").val(obj.datos[0].fk_id_pais);
+            }
+        }
+    });
+
 }
 
 </script>
