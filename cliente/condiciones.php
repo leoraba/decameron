3a26771b5a7552971074a0c3ee263c2394956065
Rene Braga <mecontreras79@gmail.com>
2014-11-23 20:13:33 -0600

    Cambios en archivos de informacin

---
+++ cliente/condiciones.php
@@ -0,0 +1,33 @@
+
+	<div class="container">
+    <div class="row">
+		<h5>Condiciones del Royal Decameron Salinitas - ALL INCLUSIVE</h5>
+		<h5>Entrada</h5>
+		<p>A partir de las 15:00</p>
+		<h5>Salida</h5>
+		<p>Hasta las 13:00</p>
+		<h5>Cancelación/prepago</h5>
+		<p>Las condiciones de cancelación y de pago por adelantado pueden variar según el tipo de habitación. Consulta las condiciones de la habitación cuando la selecciones en el menú superior.</p>
+        <h5>Condiciones sobre niños y camas supletorias</h5>
+		<p align="justify">
+Pueden alojarse niños de todas las edades.<br/>
+¡Gratis! Hasta 2 menores de 3 años se pueden alojar gratis en cunas. <br/> 
+Hasta 2 niños de 3 a 11 años se pueden alojar por 60 USD por noche utilizando las camas existentes.<br/>
+Hasta 2 niños mayores de esa edad o adultos se pueden alojar por 158 USD por noche utilizando las camas existentes.<br/>
+Número máximo de camas supletorias / cunas en la habitación: 1.<br/>
+Las camas supletorias y/o cunas están disponibles bajo petición y deben ser confirmadas por el alojamiento.<br/>
+Los suplementos no se calculan automáticamente en el importe total y deben pagarse por separado durante la estancia.<br/>
+		</p>
+		<h5>Mascotas</h5>
+		<p align="justify">No se admiten ningun tipo de mascotas en las instalaciones del hotel.</p>
+       <h5>Tarjetas aceptadas en este alojamiento</h5>
+     <p align="justify">
+     	- Visa<br/>
+     	- American Express<br/>
+     	- Master Card<br/>
+     	* NO se aceptan cheques<br/>
+	</p>
+</div>
+	</div>
+</body>
+</html>
